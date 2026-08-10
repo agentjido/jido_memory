f63f6cee9d760ac45690e311920f5b4014f4a383
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-08-10 14:00:16 -0500

    fix(release): configure git_ops

---
+++ config/config.exs
@@ -0,0 +1,21 @@
+import Config
+
+if config_env() == :dev do
+  config :git_ops,
+    mix_project: JidoMemory.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/agentjido/jido_memory",
+    manage_mix_version?: true,
+    version_tag_prefix: "v",
+    types: [
+      feat: [header: "Features"],
+      fix: [header: "Bug Fixes"],
+      perf: [header: "Performance"],
+      refactor: [header: "Refactoring"],
+      docs: [hidden?: true],
+      test: [hidden?: true],
+      deps: [hidden?: true],
+      chore: [hidden?: true],
+      ci: [hidden?: true]
+    ]
+end
